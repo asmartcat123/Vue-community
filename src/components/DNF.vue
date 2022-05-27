2ce7151058a59a6c370551941d58ebd04dd2ef3d
cc <123>
2022-05-27 13:49:20 +0800

    sfff

--- src/components/DNF.vue
+++ src/components/DNF.vue
@@ -23,7 +23,6 @@ export default {
   },
   methods:{
     async getOption(){
-      console.log(12356);
       const {data:res}=await this.$http.post("http://localhost:8081/getOption");
       if(option.yAxis.data.length==0) {
         res.forEach(item => {
